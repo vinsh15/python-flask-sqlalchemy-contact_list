49db8f5ea33f5426a828e02a2708600a4ab6db9f
Vincent <vinsh15@gmail.com>
2021-01-27 17:30:14 -0400

    aja

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,34 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/contacts', methods=['GET'])
+def handle_contact():
+    contacts = Contact.query.all()
+    response_body=[]
+    for contact in contacts:
+        response_body.append(contact.serialize())
 
     return jsonify(response_body), 200
+
+@app.route('/contacts', methods=['POST'])
+def x():
+    body = request.get_json()
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'full_name' not in body:
+        raise APIException('You need to specify the username', status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if 'address' not in body:
+        raise APIException('You need to specify the address', status_code=400)
+    if 'phone' not in body:
+        raise APIException('You need to specify the phone', status_code=400)
+
+    # at this point, all data has been validated, we can proceed to inster into the bd
+    contact1 = Contact(full_name=body['full_name'], email=body['email'], address=body['address'], phone=body['phone'])
+    db.session.add(contact1)
+    db.session.commit()
+    return "ok", 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    addres = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(14), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Contact %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
+            "address": self.addres,
+            "phone": self.phone
             # do not serialize the password, its a security breach
         }
